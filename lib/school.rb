7ab24e5f9cc78f005d2e52ad43428c5761203057
github username <github email address>
2019-12-11 17:42:16 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -14,9 +14,17 @@ class School
         roster[grade] << student_name
     end
 
-    def grade(student_name)
-        
-    end
+    def grade(student_grade)
+        roster[student_grade]
+      end
+    
+      def sort
+        array = {}
+        roster.each do |grade, students|
+          array[grade] = students.sort
+        end
+        array
+      end
 
 
 end
